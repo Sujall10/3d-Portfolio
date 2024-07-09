32cbefbf4ca2a708e12b695682650834c899d895
Sujall10 <94795609+Sujall10@users.noreply.github.com>
2024-07-09 11:09:41 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import {About, Contact , Experience, Feedback, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedback />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+import { link } from 'react-router-dom';
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
